ac941bdf26d69569c1623957403419b6c89690e2
klei22 <kauna@avalonwest.tech>
2025-07-10 07:01:58 -0700

    Update tensorboard to support more data streams
    This allows us to increase the capacity of tensorboard for viewing
    granular activation and weight data.

--- logging/start_tensorboard.sh
+++ logging/start_tensorboard.sh
@@ -3,10 +3,20 @@
 
 # Default Port
 PORT=6006
+ULIMIT=8192
 
 # Check if user provided a specific port
 if [ ! -z "$1" ]; then
   PORT="$1"
 fi
 
-tensorboard --logdir=./logs --port="$PORT" --bind_all | python3 -m tensorboard.main --logdir=./logs --port="$PORT" --bind_all
+# Check if user provided a specific port
+if [ ! -z "$2" ]; then
+  ULIMIT="$2"
+fi
+
+ulimit -n "$ULIMIT"
+
+echo "ULIMIT=${ULIMIT}; PORT=${PORT}"
+
+tensorboard --logdir=./logs --port="$PORT" --bind_all --load_fast=false || python3 -m tensorboard.main --logdir=./logs --port="$PORT" --bind_all --load_fast=false
