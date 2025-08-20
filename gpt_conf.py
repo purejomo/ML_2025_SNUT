9dc0ce418484147af116d3a67a02f3266e225664
Kauna <16511995+klei22@users.noreply.github.com>
2025-08-19 22:34:35 -0700

    Add learned confidence variations for residual scaling

--- gpt_conf.py
+++ gpt_conf.py
@@ -312,6 +312,16 @@ class GPTConfig:
     use_post_ln: bool = False
     use_pre_ln: bool = True
     use_peri_ln: bool = False
+    use_attn_resid_scaling: bool = False
+    use_mlp_resid_scaling: bool = False
+    attn_confidence_variant: str = "zeros"
+    mlp_confidence_variant: str = "zeros"
+    use_attn_resid_const: bool = False
+    attn_resid_const: float = 0.0
+    learn_attn_resid_const: bool = False
+    use_mlp_resid_const: bool = False
+    mlp_resid_const: float = 0.0
+    learn_mlp_resid_const: bool = False
 
     # Layernorm Alternatives and Options
     norm_variant_attn: str = "rmsnorm"
