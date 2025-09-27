78eed9aa5f7a95ac6572c6135580823be1fa065f
Gregory Kielian <gkielian@google.com>
2025-09-27 00:26:12 -0700

    Fix neovim install script to use x86_64
    Also add some git aliases to bash_aliases

--- dev_env_setup_scripts/06-setup-neovim.sh
+++ dev_env_setup_scripts/06-setup-neovim.sh
@@ -14,15 +14,17 @@ log() {
   echo -e "${GREEN}🚀 [$(date +'%T')] $1${NC}"
 }
 
+
+
 # --- Installation ---
 log "Installing Neovim and its configuration..."
 
 # 1. Install Neovim (latest stable) via AppImage
 log "Downloading latest Neovim AppImage..."
-wget https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-arm64.appimage
-chmod u+x nvim-linux-arm64.appimage
+wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.appimage
+chmod u+x nvim.appimage
 mkdir -p ~/.local/bin
-mv nvim-linux-arm64.appimage ~/.local/bin/nvim
+mv nvim.appimage ~/.local/bin/nvim
 log "Neovim installed to ~/.local/bin/nvim"
 
 # 2. Install dependencies
