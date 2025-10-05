7c77e6e68d3d52b24ea99c8615f115d3e1694e91
klei22 <kauna@avalonwest.tech>
2025-10-04 21:31:12 -0700

    Remove original demo file

--- analysis/high_dim_overlap/demo.sh
+++
@@ -1,6 +0,0 @@
-# Generated Cauchy clouds on S²; rotate B around Z
-python s2_plotly_rotation_demo.py \
-  --mode gen --nA 100 --nB 100 --gamma 0.2 \
-  --beta 12 --theta-deg 20 --angle-step 30 \
-  --out-html s2_overlap_plotly.html --out-csv s2_rotation_metrics.csv
-
